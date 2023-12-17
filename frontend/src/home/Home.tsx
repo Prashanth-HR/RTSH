1747db74c9f4001f411ee9c29300d6c567ea3376
Prashanth_HR <prashanth.kumar@tum.de>
2023-12-17 09:34:20 +0100

    add get certified tab

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -42,7 +42,7 @@ const NavBar = () => {
                     <Nav className="me-auto">
                         <Nav.Link href="/booking">Booking</Nav.Link>
                         <Nav.Link href="/simulation">Simulation</Nav.Link>
-                        <Nav.Link href="/donations">Donations</Nav.Link>
+                        <Nav.Link href="/get-certified">Get Certified</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
@@ -50,20 +50,13 @@ const NavBar = () => {
     )
 }
 
-const Donations = () => {
+const GetCertified = () => {
         return (
             <Alert variant="success">
               <Alert.Heading>Hey, nice to see you</Alert.Heading>
-              <p>
-                Aww yeah, you successfully read this important alert message. This
-                example text is going to run a bit longer so that you can see how
-                spacing within an alert works with this kind of content.
-              </p>
-              <hr />
-              <p className="mb-0">
-                Whenever you need to, be sure to use margin utilities to keep things
-                nice and tidy.
-              </p>
+              <div>
+                U can get your route planning software certified by us.
+              </div>
             </Alert>
           );
 }
@@ -79,7 +72,7 @@ const Body = () => {
                 <Route path='/booking/create-parking' element={<BookingForm type='parkingLot' />} />
 
                 <Route path='/booking/showbookings' element={<BookingsCalender />} />
-                <Route path='/donations' element={<Donations />} />
+                <Route path='/get-certified' element={<GetCertified />} />
 
             </Routes>
 
